3e0f7c58da59212066ad2ebf639f53c62506ca2e
Dylan <dylan.levoguer@gmail.com>
2025-05-13 00:39:55 -0700

    Completed basic game functionality
    Still need to add
    - Printed dice
    - Tiebreakers
    - Recognitions for the special combinations
    - Finalized instructions

--- a5.py
+++ a5.py
@@ -4,27 +4,104 @@
 #
 import random
 
-high_score = 0
-high_score_player = ""
-low_score = 0
-low_score_player = ""
-
-players = ["player", "cpu1", "cpu2", "cpu3"]
-
-found_winner = False
+# Constants for the game
+players = ["chosen_by_user", "CPU #1", "CPU #2", "CPU #3"]
 
 dice_ranks = {
-    1000: [1, 2, 3],
-    2000: [1, 1, 1],
+    1000: [1, 2, 3],  # LoCo!; 2 chips
+    2000: [1, 1, 1],  # Three of a kinds; 3 chip
     3000: [2, 2, 2],
     4000: [3, 3, 3],
     5000: [4, 4, 4],
     6000: [5, 5, 5],
     7000: [6, 6, 6],
-    8000: [4, 5, 6],
+    8000: [4, 5, 6],  # PoCo! 4 chips
 }
 
-chips = {
+chip_deduction = {
+    1000: 2,
+    2000: 3,
+    3000: 3,
+    4000: 3,
+    5000: 3,
+    6000: 3,
+    7000: 3,
+    8000: 4,
+}
+
+# Helper functions
+def printInBox(text):
+    """Prints the text in a box"""
+    print(f"+{'-' * (len(text) + 2)}+")
+    print(f"| {text} |")
+    print(f"+{'-' * (len(text) + 2)}+")
+
+def calculate_score(roll):
+    """Calculate the score for a given roll"""
+    score = 0
+    for num in roll:
+        if num == 1:
+            score += 100
+        elif num == 6:
+            score += 60
+        else:
+            score += num
+    return score
+
+def roll_dice(num_rolls, player):
+    """Rolls the dice for the player and returns the num of rolls"""
+    roll = [random.randint(1, 6) for _ in range(3)]
+    times_rolled = 1
+    
+    while times_rolled < num_rolls:
+        # Check if the roll is a special combination
+        if roll in dice_ranks.values():
+            break
+
+        # Calculate the current score
+        score = calculate_score(player_rolls[player])
+
+        # Stop rolling if the score is between 100 and 200
+        if 200 > score > 100:
+            break
+            
+        dice_to_reroll = []
+        
+        # Check if dice should be rerolled
+        for i, die in enumerate(roll):
+            if die == 1 or die == 6:
+                continue
+            if roll.count(die) >= 2:
+                continue
+            dice_to_reroll.append(i)
+
+        for i in dice_to_reroll:
+            roll[i] = (random.randint(1, 6))
+
+        times_rolled += 1
+
+        if roll in dice_ranks.values():  # Check again if the roll is a special combination
+            break
+
+    roll.sort()  # Sort the roll to make it easier to compare
+    player_rolls[player] = roll
+    print(f"{player} rolled {player_rolls[player]}")
+    return times_rolled
+
+# Game setup
+printInBox("Welcome to PoCoLoCo!")
+print(
+    "Rules: \n"
+    "\t1. The first player can roll up to 3 dice \n"
+    "\t2. Each roll, the player can choose to stop or to reroll up to 3 of their dice \n"
+    "\t3. The player with the lowest score gains chips from the other players \n"
+    "\t4. The first player to run out of chips is the winner"
+)
+
+# Ask the player for their name
+players[0] = input("\nWhat is your name?: ")
+
+chip_count = {
     # Define the chips for each player
     players[0]: 10,
     players[1]: 10,
@@ -40,40 +117,95 @@ player_rolls = {
     players[3]: [],
 }
 
-for players in chips:
-    print(f"{players}")
-    print(f"Chips: {chips[players]}")
+# Ask the player for the number of chips everyone starts with
+num_chips = int(input("\nHow many chips do you want to start with?: "))
+for player in chip_count.keys():
+    chip_count[player] = num_chips
 
+# Main Game Loop
+round = 0
+found_winner = False
+while not found_winner:
+    """Runs the game until a player wins by running out of chips"""
 
-# roll dice for player
-def roll_dice():
-    roll = []
-    for _ in range(3):
-        roll.append(random.randint(1, 6))
-    roll.sort()
-    return roll
+    # Prints the round number
+    printInBox("Round " + str(round + 1))
 
+    # Reset high and low scores
+    high_score = 0
+    high_score_player = None
+    low_score = float("inf")
+    low_score_player = None
 
-for player in player_rolls:
-    score = 0
-    player_rolls[player] = roll_dice()
-    if player_rolls[player] in dice_ranks.values():
-        score = dice_ranks
-    # if player_rolls[player] == dice_ranks[7]:
-    #     print(f"{player} rolled a PoCo!")
-    #     high_score_player = player
-    # elif player_rolls[player] == dice_ranks[0]:
-    #     print(f"{player} rolled a LoCo!")
-    # elif player_rolls[player] in dice_ranks.values:
-    #     player_rolls[player]
+    # Reset max rolls
+    num_rolls = 3
+    last_num_rolls = num_rolls
+    # Reset player rolls
+    for player in player_rolls:
+        player_rolls[player] = []
+
+    for player in player_rolls:
+        score = 0
+
+        last_num_rolls = roll_dice(last_num_rolls, player)
+
+        # Check if player_rolls[player] is a special combination
+        for key, value in dice_ranks.items():
+            if player_rolls[player] == value:
+                score = key
+                break
+        else:
+            score = calculate_score(player_rolls[player])
+
+        # Set high and low scores
+        if score > high_score:
+            high_score = score
+            high_score_player = player
+        if score < low_score:
+            low_score = score
+            low_score_player = player
+        round += 1
+
+    # Determine how many chips to be taken from the winners
+    if high_score in chip_deduction:
+        taken_chips = chip_deduction.get(high_score, 0)
     else:
-        for num in player_rolls[player]:
-            if num == 1:
-                score += 100
-            elif num == 6:
-                score += 60
-            else:
-                score += num
+        taken_chips = 1  # Num of chips to be taken if not in the ranks
 
+    # Deducts chips from the winners and adds them to the loser
+    for player in chip_count:
+        if player == low_score_player:
+            chip_count[player] += (
+                taken_chips * len(players) - 1
+            )  # chip(s) taken from each player and given to the loser
+        else:
+            chip_count[player] -= taken_chips
 
-print(player_rolls)
+    print(f"High score: {high_score_player} with {high_score}")
+    print(f"Low score: {low_score_player} with {low_score}")
+
+    print("Chips:")
+    for player, chip in chip_count.items():
+        if chip < 0:
+            chip = 0
+        print(player, "has", chip, "chips")
+
+    for player, roll in player_rolls.items():
+        print(player, "rolled", roll)
+
+    for player in chip_count:
+        if chip_count[player] <= 0:
+            winner = player
+            found_winner = True
+            break
+        
+# Endgame Logic
+if player_rolls[winner] in dice_ranks.values():
+    if player_rolls[winner] == dice_ranks[1000]:
+        print(f"{winner}wins the game with a LoCo!")
+    elif player_rolls[winner] == dice_ranks[8000]:
+        print(f"{winner} wins the game with a PoCo!")
+    else:
+        print(f"{winner} wins the game with a Three of a Kind!")
+else:
+    print(f"{winner} wins the game with the highest score!")
